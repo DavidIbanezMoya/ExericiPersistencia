030752f922212530ff4a947f25606138575d8b2b
DavidIbanezMoya <davidibanezmoya@gmail.com>
2022-11-04 19:24:38 +0100

    We create the new file

--- app/src/main/java/com/example/exercicipersistencia/MainActivity.java
+++ app/src/main/java/com/example/exercicipersistencia/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.exercicipersistencia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Button boto = (findViewById(R.id.button));
+        try {
+            OutputStream os = this.openFileOutput("dades.txt", MODE_PRIVATE);
+            String text = "Missatge de proba";
+            os.write(text.getBytes());
+            os.close();
+
+            File arxiu = this.getFilesDir();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
